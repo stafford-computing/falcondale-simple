8fca3ec8b3e5884faaa1ae8e063457402e84b747
Fabio Beider <fabeider@gmail.com>
2026-02-03 22:05:33 -0300

    fix: set basename for BrowserRouter in App component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
